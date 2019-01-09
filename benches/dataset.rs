945991192c3bd2bae1da86570e4bc664fc75c401
Suyash <suyash93@protonmail.com>
2019-01-09 11:37:56 +0530

    benches: add benchmarks for loading MNIST

--- benches/dataset.rs
+++ benches/dataset.rs
@@ -7,14 +7,14 @@ use test::{black_box, Bencher};
 use datasets::Dataset;
 
 #[bench]
-fn bench_shuffle(b: &mut Bencher) {
+fn dataset_shuffle(b: &mut Bencher) {
     b.iter(|| {
         black_box((0..2048).shuffle(256, 0).collect::<Vec<usize>>());
     });
 }
 
 #[bench]
-fn bench_batch(b: &mut Bencher) {
+fn dataset_batch(b: &mut Bencher) {
     b.iter(|| {
         black_box((0..2048).batch(255).collect::<Vec<Vec<usize>>>());
     });
---
+++ benches/mnist.rs
@@ -0,0 +1,69 @@
+#![feature(test)]
+
+extern crate test;
+
+use std::fs;
+use std::path::Path;
+
+use test::{black_box, Bencher};
+
+use datasets::image::mnist;
+
+#[bench]
+fn mnist_load_from_scratch(b: &mut Bencher) {
+    let path = Path::new("./tmp/mnist");
+    match path.metadata() {
+        Ok(d) => {
+            if d.is_dir() {
+                fs::remove_dir_all(path).unwrap();
+            } else {
+                fs::remove_file(path).unwrap();
+            }
+
+            fs::create_dir_all(path).unwrap();
+        }
+        Err(_) => fs::create_dir_all(path).unwrap(),
+    }
+
+    b.iter(|| {
+        black_box(mnist::load(path).unwrap());
+    });
+}
+
+#[bench]
+fn mnist_load_from_cache(b: &mut Bencher) {
+    let path = Path::new("./tmp/mnist");
+    let _ = mnist::load(path).unwrap();
+
+    b.iter(|| {
+        black_box(mnist::load(path).unwrap());
+    });
+}
+
+#[bench]
+fn mnist_load_train_dataset_full(b: &mut Bencher) {
+    let path = Path::new("./tmp/mnist");
+
+    // make sure the dataset is downloaded
+    let _ = mnist::load(path).unwrap();
+
+    b.iter(|| {
+        let (train_dataset, _) = mnist::load(path).unwrap();
+        let v = train_dataset.collect::<Vec<(Vec<Vec<u8>>, u8)>>();
+        v.len()
+    });
+}
+
+#[bench]
+fn mnist_load_test_dataset_full(b: &mut Bencher) {
+    let path = Path::new("./tmp/mnist");
+
+    // make sure the dataset is downloaded
+    let _ = mnist::load(path).unwrap();
+
+    b.iter(|| {
+        let (_, test_dataset) = mnist::load(path).unwrap();
+        let v = test_dataset.collect::<Vec<(Vec<Vec<u8>>, u8)>>();
+        v.len()
+    });
+}
